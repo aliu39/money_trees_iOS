7a481fbcbe8e0c4fabec7b922d2153b3f99f946a
Andrew Liu <aliu.13119@gmail.com>
2021-01-06 20:27:24 -0800

    Initial Commit

---
+++ money_trees/money_treesApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_treesApp.swift
+//  money_trees
+//
+//  Created by Andrew Liu on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct money_treesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
